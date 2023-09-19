2e35e6d968b59940f18c7af30d699ce636275453
thomasmendonca <rdcharacter10@gmail.com>
2023-09-18 23:02:44 -0300

    Avançando em Rotas e fazendo a Page Sizes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import Flavours from "./pages/flavours/Flavours"
 import Summary from "./pages/summary/Summary"
 import Checkout from "./pages/checkout/Checkout"
 import Login from "./pages/login/Login"
+import { routes } from "./routes"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pedido/escolha-o-tamanho" element={<Sizes />} />
-        <Route path="/pedido/escolha-o-sabor" element={<Flavours />} />
-        <Route path="/pedido/resumo" element={<Summary />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/login" element={<Login />} />
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.pizzaSize} element={<Sizes />} />
+        <Route path={routes.pizzaFlavour} element={<Flavours />} />
+        <Route path={routes.summary} element={<Summary />} />
+        <Route path={routes.checkout} element={<Checkout />} />
+        <Route path={routes.login} element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/sizes/Sizes.tsx
+++ src/pages/sizes/Sizes.tsx
@@ -1,3 +1,76 @@
+import { Button } from "../../components/button/Button";
+import { Layout } from "../../components/layout/Layout";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom"
+import { routes } from "../../routes"
+
 export default function Sizes() {
-  return <h1>Sizes</h1>
+  const navegate = useNavigate()
+  const sizeOptions = [
+  {
+    id: 10,
+    value: 'large',
+    text: 'Grande',
+  },
+  {
+    id: 11,
+    value: 'large-2',
+    text: 'Grande 2 sabores',
+  },
+  {
+    id: 20,
+    value: 'medium',
+    text: 'Média',
+  },
+  {
+    id: 21,
+    value: 'medium-2',
+    text: 'Média 2 sabores',
+  },
+  {
+    id: 30,
+    value: 'small',
+    text: 'Broto',
+  },
+  {
+    id: 31,
+    value: 'small-2',
+    text: 'Broto 2 Sabores',
+  },
+]
+
+  const [size, setSize] = useState("")
+
+  const handleChange = (event) =>{
+    setSize(event.target.id)
+  }
+  const handleBack = () =>{
+    navegate(routes.home)
+  }
+  const handleNext = () =>{
+    navegate(routes.pizzaFlavour)
+  }
+
+  return (
+    <Layout>
+      <h1>Escolha o tamanho da pizza</h1>
+      <section>
+        {sizeOptions.map(({id,value,text})=>(
+          <div key={id}>
+           <input type="radio" 
+           id={value} 
+           name="sizes" 
+           onChange={handleChange}
+           value={size}
+           />
+           <label htmlFor={value}>{text}</label>
+          </div>
+        ))}
+      </section>
+      <div>
+        <Button onClick={handleBack}>Voltar</Button>
+        <Button onClick={handleNext}>Escolha o sabor</Button>
+      </div>    
+    </Layout>
+  )
 }
