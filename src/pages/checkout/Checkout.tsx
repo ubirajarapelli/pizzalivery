2a394ca7537261cde07600daeb87af906c1ca932
Thiago Zupelli <thiago_zupelli@hotmail.com>
2023-10-09 23:22:28 -0300

    added async/await api requests

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -25,7 +25,7 @@ export default function Checkout() {
   ]
 
   const [paymentType, setPaymentType] = useState("")
-  const [isDisabled, setIsDisabled] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleChange = (event) => {
     setPaymentType(event.target.value)
@@ -41,9 +41,71 @@ export default function Checkout() {
     return filteredValue[0].value
   }
 
-  const handleClick = () => {
-    alert("Pedido feito")
+  // const handleClick = () => {
+  //   setIsLoading(true)
+    
+  //   fetch('https://www.api.pizzalivery.com.br/order', {
+  //     method: "POST",
+  //     body: JSON.stringify(pizzaOrder),
+  //   })
+  //     .then(() => {
+  //       alert("Deu bom")
+  //     })
+  //     .catch(() => {
+  //       console.log("Deu ruim")
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false)
+  //       console.log("Finalizou")
+  //     })
+  // }
+
+  // async/await
+  const doOrder = async () => {
+    setIsLoading(true)
+
+    try{
+      const response = await fetch('https://www.api.pizzalivery.com.br/order', 
+          {
+            method: "POST",
+            body: JSON.stringify(pizzaOrder),
+          }
+        )
+
+        console.log(response)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setIsLoading(false)
+    }
   }
+
+  // //  METHODS/VEBS HTTP
+  // //  GET: Receber uma informação; √
+  // fetch("https://www.api.pizzalivery.com.br/order")
+
+  // const filter = "coca cola"
+  // fetch(`https://www.api.pizzalivery.com.br/bebidas=${filter}`)
+
+  // //  POST: Enviar uma informação; √
+  // fetch("https://www.api.pizzalivery.com.br/order", {
+  //   method: "POST",
+  //   body: JSON.stringify(pizzaOrder),
+  // })
+
+  // //  PUT: Alterar uma informação; √
+  // fetch("https://www.api.pizzalivery.com.br/order", {
+  //   method: "PUT",
+  //   body: JSON.stringify(pizzaOrder),
+  // })
+
+  // //  DELETE: Remover uma informação;
+  // const orderID = 12345100;
+
+  // fetch(`https://www.api.pizzalivery.com.br/order/${orderID}`, {
+  //   method: "DELETE",
+  // })
+
 
   useEffect(() => {
     if (pizzaOrder === undefined) {
@@ -55,44 +117,50 @@ export default function Checkout() {
 
   return (
     <Layout>
-      <Title tabIndex={0}>Checkout</Title>
-      <CheckoutItem>
-        <h2>Items</h2>
-        <CheckoutItemFlex>
-          <p>
-            {pizzaOrder?.item.name} - {pizzaOrder?.item.size}
-          </p>
-          <p>
-            {convertToCurrency(pizzaOrder?.item.value)}
-          </p>
-        </CheckoutItemFlex>
-      </CheckoutItem>
-      <CheckoutItem>
-        <h2>Forma de Pagamento</h2>
-        <CheckoutItemFlex>
-          <PaymentMethodGroup>
-            <label htmlFor="payments">Selecione a forma de pagamento</label>
-            <select name="payments" id="payments" defaultValue={""} onChange={handleChange}>
-              <option disabled value="">Selecione</option>
-                {paymentOptions.map(({ id, value, text}) => (
-                <option key={id} value={value}>
-                  {text}
-                </option>
-              ))}
-            </select>
-          </PaymentMethodGroup>
-          <p>{getPaymentOptionType(Number(paymentType))}</p>
-        </CheckoutItemFlex>
-      </CheckoutItem>
-      <CheckoutItem>
-        <CheckoutItemFlex>
-          <h2>Total do pedido</h2>
-          <p>{convertToCurrency(pizzaOrder?.total)}</p>
-        </CheckoutItemFlex>
-      </CheckoutItem>
-      <CheckoutAction>
-        <Button onClick={handleClick} disabled={!Boolean(paymentType)}>Fazer pedido</Button>
-      </CheckoutAction>
+      {isLoading ? (
+        <>Carregando</>
+      ) : (
+        <>
+          <Title tabIndex={0}>Checkout</Title>
+          <CheckoutItem>
+            <h2>Items</h2>
+            <CheckoutItemFlex>
+              <p>
+                {pizzaOrder?.item.name} - {pizzaOrder?.item.size}
+              </p>
+              <p>
+                {convertToCurrency(pizzaOrder?.item.value)}
+              </p>
+            </CheckoutItemFlex>
+          </CheckoutItem>
+          <CheckoutItem>
+            <h2>Forma de Pagamento</h2>
+            <CheckoutItemFlex>
+              <PaymentMethodGroup>
+                <label htmlFor="payments">Selecione a forma de pagamento</label>
+                <select name="payments" id="payments" defaultValue={""} onChange={handleChange}>
+                  <option disabled value="">Selecione</option>
+                    {paymentOptions.map(({ id, value, text}) => (
+                    <option key={id} value={value}>
+                      {text}
+                    </option>
+                  ))}
+                </select>
+              </PaymentMethodGroup>
+              <p>{getPaymentOptionType(Number(paymentType))}</p>
+            </CheckoutItemFlex>
+          </CheckoutItem>
+          <CheckoutItem>
+            <CheckoutItemFlex>
+              <h2>Total do pedido</h2>
+              <p>{convertToCurrency(pizzaOrder?.total)}</p>
+            </CheckoutItemFlex>
+          </CheckoutItem>
+          <CheckoutAction>
+            <Button onClick={doOrder} disabled={!Boolean(paymentType)}>Fazer pedido</Button>
+          </CheckoutAction>
+        </>
+      )}
     </Layout>
   ) 
 }
